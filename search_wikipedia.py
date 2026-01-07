5ad09d0240c10f45c7c7fb028f3e84e413a8f67d
Nikunj Thakur <nik.thak22@gmail.com>
2026-01-07 17:17:03 +0000

    first commit

---
+++ search_wikipedia.py
@@ -0,0 +1,13 @@
+# install wikipedia package if not already installed
+# pip install wikipedia
+import wikipedia
+
+topic = input("Enter a topic to search: ")
+print("=" * 40)
+print(f"Searching Wikipedia for: {topic}")
+print("=" * 40)
+
+res = wikipedia.summary(topic, sentences=3)
+
+print(res)
+print("=" * 40)
